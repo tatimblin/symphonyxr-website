a3feb5228f91b0e87a5d2d5ddc8fdadfac347185
Tristan Timblin <tatimblin@gmail.com>
2018-01-10 22:50:32 -0500

    Static project timeline.

--- wp-content/themes/symphony-xr/page-homepage.php
+++ wp-content/themes/symphony-xr/page-homepage.php
@@ -12,6 +12,8 @@ get_header(); ?>
     <?php get_template_part( 'template-parts/content', 'hero' ); ?>
             
 	<?php get_template_part( 'template-parts/content', 'about' ); ?>
+	
+	<?php get_template_part( 'template-parts/content', 'timeline' ); ?>
 
 	</div><!-- #main -->
 
---
+++ wp-content/themes/symphony-xr/template-parts/content-timeline.php
@@ -0,0 +1,30 @@
+<div class="timeline">
+    <div class="timeline-heading">
+        <div class="timeline-heading-title">
+            <h2>Project Timeline</h2>
+        </div>
+        <div class="timeline-heading-cta">
+            <a class="button" href="">Weekly Journals</a>
+        </div>
+    </div>
+    <div class="timeline-bar">
+        <div class="timeline-bar-item tl-complete">
+            <p>
+                <span>January</span>
+                Shovel Snow
+            </p>
+        </div>
+        <div class="timeline-bar-item">
+            <p>
+                <span>March</span>
+                Go To The Beach
+            </p>
+        </div>
+        <div class="timeline-bar-item">
+            <p>
+                <span>June</span>
+                The Finish Line
+            </p>
+        </div>
+    </div>
+</div>
